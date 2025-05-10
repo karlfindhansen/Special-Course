f9a076be62228e6c4ad89122576a63ae44cca060
karlfindhansen <karl.find.hansen@gmail.com>
2025-05-10 15:39:35 +0200

    Updated train to make correct plots

--- src/train.py
+++ src/train.py
@@ -14,13 +14,13 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 from scipy.interpolate import make_interp_spline
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 sys.path.append('comparison')
 sys.path.append('data')
 sys.path.append('src/Model')
 
 from data_preprocessing import ArcticDataloader
-#from compare_gen_bedmap import regions_of_interest
 from GeneratorModel import GeneratorModel
 from DiscriminatorModel import DiscriminatorModel
 
@@ -29,7 +29,7 @@ def train(
     learning_rate=0.0006921253577657427,
     num_residual_blocks=12,
     residual_scaling=0.3,
-    epochs=100,
+    epochs=250,
 ):
     # Load dataset
     dataset = ArcticDataloader(
@@ -209,24 +209,71 @@ def save_specified_area(imgs, preds):
     else:
         df = pd.DataFrame(columns=["image_id", "crop_y1", "crop_y2", "crop_x1", "crop_x2", "image_filename"])
 
+    preds = preds.squeeze(1).cpu().numpy()
+
+    grid_size = int(np.sqrt(len(preds)))
+    pred_height, pred_width = preds.shape[1], preds.shape[2]
+    canvas_pred = np.zeros((grid_size * pred_height, grid_size * pred_width))
+    canvas_bedmachine = canvas_pred.copy()
+    canvas_difference = canvas_pred.copy()
+    
     for i in range(len(preds)):
+
+        row = i // grid_size
+        col = i % grid_size
+
+        y1, y2 = row * pred_height, (row + 1) * pred_height
+        x1, x2 = col * pred_width, (col + 1) * pred_width
+
         crop_y1, crop_y2 = int(imgs['crops']['Original']['y_1'][i]), int(imgs['crops']['Original']['y_2'][i])
         crop_x1, crop_x2 = int(imgs['crops']['Original']['x_1'][i]), int(imgs['crops']['Original']['x_2'][i])
-        #if (crop_y1 >= org_y1 and crop_y2 <= org_y2) and (crop_x1 >= org_x1 and crop_x2 <= org_x2):
-        pred_img = preds[i].detach().cpu().numpy()
-        pred_img = np.squeeze(pred_img)
+
+        canvas_pred[y1:y2, x1:x2] = preds[i]
+        canvas_bedmachine[y1:y2, x1:x2] = hr_bed_machine[crop_y1: crop_y2, crop_x1:crop_x2]
+        canvas_difference[y1:y2,x1:x2] = preds[i] - hr_bed_machine[crop_y1: crop_y2, crop_x1:crop_x2]
 
         num_imgs_in_folder = len(os.listdir(save_path))
-
-        image_filename = f'pred_{num_imgs_in_folder+1}.png'
-        plt.imsave(f'{save_path}{image_filename}', pred_img, cmap='terrain', vmin=vmin, vmax=vmax)
 
         records.append({
             "image_id": i,
             "crop_y1": crop_y1, "crop_y2": crop_y2,
             "crop_x1": crop_x1, "crop_x2": crop_x2,
-            "image_filename": image_filename
+            "image_filename": f'pred_{num_imgs_in_folder+1}.png'
         })
+
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+
+    plt.imsave(f'{save_path}pred_grid.png', canvas_pred, cmap='terrain', vmin=vmin, vmax=vmax)
+    plt.imsave(f'{save_path}pred_grid_bedmachine.png', canvas_bedmachine, cmap='terrain', vmin=vmin, vmax=vmax)
+    plt.imsave(f'{save_path}pred_minus_bedmachine.png', canvas_difference, cmap='cividis')
+
+    # Plot 1: Predicted Image Grid
+    im1 = axes[0].imshow(canvas_pred, cmap='terrain', vmin=vmin, vmax=vmax)
+    axes[0].set_title("Predicted Elevation")
+    axes[0].axis('off')
+    divider1 = make_axes_locatable(axes[0])
+    cax1 = divider1.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im1, cax=cax1, label='Elevation [m]')
+
+    # Plot 2: Bedmachine Data Grid
+    im2 = axes[1].imshow(canvas_bedmachine, cmap='terrain', vmin=vmin, vmax=vmax)
+    axes[1].set_title("Bedmachine Elevation")
+    axes[1].axis('off')
+    divider2 = make_axes_locatable(axes[1])
+    cax2 = divider2.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im2, cax=cax2, label='Elevation [m]')
+
+    # Plot 3: Difference Grid
+    im3 = axes[2].imshow(canvas_difference, cmap='cividis')
+    axes[2].set_title("Difference (Predicted - Bedmachine)")
+    axes[2].axis('off')
+    divider3 = make_axes_locatable(axes[2])
+    cax3 = divider3.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im3, cax=cax3, label='Elevation Difference [m]')
+
+    plt.tight_layout()
+    plt.savefig(f'{save_path}comparison_grid.png', dpi=300, bbox_inches='tight')
+    plt.close()
 
     new_df = pd.DataFrame(records)
     df = pd.concat([df, new_df], ignore_index=True)
--- src/train.sh
+++ src/train.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #BSUB -J train
-#BSUB -q hpc
-#BSUB -W 20
-#BSUB -R "rusage[mem=8192MB]"
-#BSUB -n 2         
+#BSUB -q gpua100
+#BSUB -W 300
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -n 3         
 #BSUB -R "span[hosts=1]"   
-#BSUB -o src/batch_out/sleeper_%J.out
-#BSUB -e src/batch_out/sleeper_%J.err 
-#BSUB -N  
+#BSUB -o src/batch_out/train_%J.out
+#BSUB -e src/batch_out/train_%J.err 
 
 source '../special_course/bin/activate'
 
